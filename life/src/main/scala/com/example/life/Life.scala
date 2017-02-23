7432fdb97e1505d044d4be9064a37f2d52ca5b83
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2017-02-23 20:12:56 +0200

    step-0

--- life/src/main/scala/com/example/life/Life.scala
+++ life/src/main/scala/com/example/life/Life.scala
@@ -1,15 +1,29 @@
 package com.example.life
 
+case class Point(x:Int,y:Int)
+{
+  def applyOffset(dx:Int,dy:Int)=Point(x+dx,y+dy)
+}
 
 trait Life {
 
 
+
   trait Field {
 
-    def isAlive(x:Int,y:Int):Boolean
+    /**
+      * Assume that x & y can be bigger that actual fiedl
+      */
+    def isAlive(p:Point):Boolean
 
-    def nextAction(x:Int,y:Int): Boolean =
+    def nextAction(p:Point): Boolean =
     {
+      val n = neighbourOffsets.map{ case (dx,dy) =>
+          p.applyOffset(dx,dy)
+        }.filter(isAlive(_)).size
+
+      
+
       ???
     }
 
@@ -19,5 +33,12 @@ trait Life {
 
 
   var current: Field
+
+
+  val neighbourOffsets = Set(
+    (-1,-1),(-1,0),(-1,1),
+    (0 ,-1),       (0 ,1),
+    (1, -1),(1 ,0),(1 ,1)
+  )
 
 }
