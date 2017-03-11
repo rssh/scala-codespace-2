9a6003b8a352d27ab0d313f1a80bdf1a3bd7de3c
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2017-03-11 13:18:57 +0200

    initial

---
+++ currencyBoard/src/main/scala/com/example/CurrencyBoard.scala
@@ -0,0 +1,69 @@
+package com.example
+
+import scala.concurrent.Future
+
+
+case class Money(amount: BigDecimal,
+                 currency: Symbol)
+
+case class CurrencyRate(ask:BigDecimal,
+                        bid:BigDecimal,
+                        amount:BigDecimal,
+                        symbol:Symbol,
+                        baseSymbol: Symbol)
+
+
+
+trait CurrencyBoard {
+
+
+  // def ,... A => M[B],    Kleisly(M,A,B)
+
+  def rates():Future[Seq[CurrencyRate]]
+
+  def baseSymbols():Future[CurrencyRate]
+
+  def exchange(from:Symbol,to:Symbol,fromAmount:BigDecimal):Future[Either[String,BigDecimal]]
+
+}
+
+object X
+{
+
+  val xPortfilio = List(1.0 ->'USD,1.0->'EUR,300.0 ->'VON, 0.4->'BTC) map {
+    case (x,currency) => Money(x,currency)
+  }
+
+  def gatherHRN(portfilio:List[(Money,Symbol)],
+                currencyBoard:CurrencyBoard):Future[Either[String,Money]] =
+  {
+    val state: Future[Either[String,Money]] = Future successful Right(Money(0.0,'HRN))
+    xPortfilio.foldLeft(state){ (sFuture,eFuture) =>
+      for( sE <- sFuture;
+           eE <- eFuture;
+           nE <- eitherToHrn(eE,currencyBoard)) yield eitherSum(sE,nE)
+    }
+  }
+
+  def eitherToHrn(eE:Either[String,Money], currencyBoard: CurrencyBoard): Future[Either[String,Money]] = {
+    eE match {
+      case Left(e) => Future successful(eE)
+      case Right(e) => toHrn(e,currencyBoard)
+    }
+  }
+
+  def eitherSum(sE:Either[String,Money],
+                eE:Either[String,Money]):Either[String,Money] = {
+    //for {s <- sE
+    //     e <- eE} yield Money(s.amount + e.amount, s.currency)
+    //sE.flatMap(s => for{e <- eE} yield Money(s.amount + e.amount, s.currency) )
+    sE.flatMap(s => eE.map(e => Money(s.amount + e.amount,s.currency)))
+  }
+
+
+  def toHrn(in:Money, currencyBoard: CurrencyBoard):Future[Either[String,Money]] =
+    for(ex <- currencyBoard.exchange(in.currency,'HRN,in.amount)) yield {
+      for(x <- ex) yield Money(x,'HRN)
+    }
+
+}
---
+++ currencyBoard/src/main/scala/com/example1/CurrencyBoard.scala
@@ -0,0 +1,47 @@
+package com.example1
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+case class Task[A](fex:Future[Either[String,A]])
+{
+
+
+  def map[B](f:A=>B):Task[B] =
+    Task{ fex.map{ ex => ex.map(f) }}
+
+  def flatMap[B](f: A=>Task[B]):Task[B] = {
+    val xf: Future[Either[String,Task[B]]] = fex map { ex => ex.map(f) }
+    // TODO:
+    //   // tailRecM
+    ???
+    //xf.flatMap(xfe => xfe.flatMap(t=>t.inner))
+
+    /*
+    for{ex <- fex
+         taskB = ex map f
+         feb = taskB.inner
+         // TODO
+         */
+
+  }
+
+  //def flatten
+
+  def inner = fex
+
+
+  def unsafeRun():A = {
+    val ex = Await.result(fex,1 minute)
+    ex match {
+      case Right(v) => v
+      case Left(x) => throw new IllegalStateException(x)
+    }
+  }
+
+}
+
+
+class CurrencyBoard {
+
+}
